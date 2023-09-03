1a40e1e30073f5f4358505f58a85b8b7457eb75e
etctera <yilonglei790@gmail.com>
2023-09-03 17:34:34 +0800

    feat: get_rust_doc with commant args

--- get_rust_doc/src/main.rs
+++ get_rust_doc/src/main.rs
@@ -1,15 +1,22 @@
-use std::fs;
+use get_rust_doc::Config;
+use std::{env, fs, process};
 
 fn main() {
-    let url = "https://www.rust-lang.org/";
-    let output = "rust.md";
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err); // 打印错误信息
+        process::exit(1); // 退出程序，返回错误状态码
+    });
+    println!("{:?}", config);
 
-    println!("Fetching url: {}", url);
-    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+    println!("Fetching url: {}", config.url);
+    let body = reqwest::blocking::get(config.url).unwrap().text().unwrap();
 
     println!("Converting html to markdown...");
     let md = html2md::parse_html(&body);
 
-    fs::write(output, md.as_bytes()).unwrap();
-    println!("Converted markdown has been saved in {}.", output);
+    fs::write(&config.output_path, md.as_bytes()).unwrap();
+    println!(
+        "Converted markdown has been saved in {}.",
+        config.output_path
+    );
 }
